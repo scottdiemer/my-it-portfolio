25d97754f0b6db68541830531b28be54e93b284c
Scott Diemer <scottdiemer@gmail.com>
2026-07-03 13:34:31 +0100

    feat: add automated bash deployment script

---
+++ nextjs-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit immidiately if a command exits with an error
+set -e
+
+echo "Starting Next.js deployment..."
+
+echo "Pulling latest updates from github..."
+git pull origin main
+
+# Check if package.json changed, if so install new packages
+# (Using --legacy-peer-deps to bypass React/Next version conflict)
+echo "Installing any new dependencies..."
+npm install --legacy-peer-deps
+
+echo "Build the production Next.js application..."
+npm run build
+
+# Restart the application
+pm2 restart my-it-portfolio
+
+echo "Deployment completed successfully!"
